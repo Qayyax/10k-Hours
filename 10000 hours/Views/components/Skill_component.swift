61c3df2737b7ab9d0953a2ee5f06cbe4ac5231c2
Qayyax <qayyax@gmail.com>
2026-02-06 21:27:11 -0500

    before variable background

--- 10000 hours/Views/components/Skill_component.swift
+++ 10000 hours/Views/components/Skill_component.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct Skill_component: View {
+    var progress: CGFloat = 0.6
     var body: some View {
         HStack{
             VStack {
@@ -11,14 +12,17 @@ struct Skill_component: View {
             Spacer()
         }
         .frame(maxWidth: .infinity)
-        .background(.blue)// would change this to overlay of progress
-        .clipShape(RoundedRectangle(cornerRadius: 20))
-        .overlay(alignment: .bottomTrailing){
-            Text("3 hrs left")
-                .padding(.bottom)
-                .padding(.trailing)
-        }
-        // another overlay for color
+        .background(in: RoundedRectangle(cornerRadius: 20))
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(.black, lineWidth: 5)
+            )
+            .overlay(alignment: .bottomTrailing){
+                Text("3 Hrs/ 10,000 Hrs")
+                    .padding(.bottom)
+                    .padding(.trailing)
+            }
+            // another overlay for progress color
     }
 }
 
