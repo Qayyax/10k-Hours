44b9c9aeaaeaa3706d7344e35e62541c3fd4cfc0
Qayyax <qayyax@gmail.com>
2026-02-08 01:19:11 -0500

    new file

---
+++ 10000 hours/Views/Skills/NoSkillsView.swift
@@ -0,0 +1,22 @@
+//
+//  NoSkillsView.swift
+//  10000 hours
+//
+//  Created by Abdul-Qayyum Olatunji on 2026-02-08.
+//
+
+import SwiftUI
+
+struct NoSkillsView: View {
+    var body: some View {
+        ZStack {
+            VStack{
+                Text("No Skills Yet")
+            }
+        }
+    }
+}
+
+#Preview {
+    NoSkillsView()
+}
