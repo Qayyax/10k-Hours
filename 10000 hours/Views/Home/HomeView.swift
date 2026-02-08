e27d7b0687cf29cfa835f33333b854d766825f27
Qayyax <qayyax@gmail.com>
2026-02-07 23:04:09 -0500

    added if statement for when there is no skill

--- 10000 hours/Views/Home/HomeView.swift
+++ 10000 hours/Views/Home/HomeView.swift
@@ -1,31 +1,10 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var noSkill = true
     var body: some View {
-        NavigationStack {
-            VStack {
-                // Here would be the list of skills components
-                VStack {
-                    // There should probably be a rate limiting of how many skills
-                    // show up here
-                    Text("Drawing")
-                    Text("Knitting")
-                    Text("Cooking")
-                }
-                // here would be another vStack of logs
-                VStack {
-                    Text("Logs")
-                }
-                // Might add music player, don't know what to put at the bottom yet
-                Spacer()
-            }
-            .padding()
-            .navigationTitle("Home")
-            .toolbar {
-                ToolbarItem(placement: .automatic) {
-                    Text("Timer component")
-                }
-            }
+        if noSkill {
+            NoSkillView()
         }
     }
 }
