472b1b278025625e01348b3f1fac942c37972a22
Qayyax <qayyax@gmail.com>
2026-02-08 12:56:54 -0500

    added is no skills

--- 10000 hours/Views/Skills/Skills_View.swift
+++ 10000 hours/Views/Skills/Skills_View.swift
@@ -2,49 +2,12 @@ import SwiftUI
 
 struct Skills_View: View {
     @State private var progress = 0.5
+    @State private var isSkillAvailable = false
     var body: some View {
-        ZStack(alignment: .leading) {
-            VStack(alignment: .leading) {
-                // Progressive View component
-                VStack(alignment: .leading) {
-                    Text("3/ 10")
-                    ProgressView(value: progress)                         .progressViewStyle(GaugeProgressStyle())
-                }
-                .padding()
-                .frame(maxWidth: .infinity)
-                .overlay{
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(.black, lineWidth: 5)
-                }
-                
-                // Reason for Practice component
-                VStack(alignment: .leading){
-                    Text("Reason for Practice")
-                        .font(.title2.bold())
-                    VStack {
-                        Text("I am practicing how to code because ...")
-                        Text("I am practicing how to code because ...")
-                        Text("I am practicing how to code because ...")
-                        // the text should show likw three lines
-                        // when you click it, it should then beable to open a .sheet so you can see the whole thing
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .overlay{
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(.black, lineWidth: 5)
-                    }
-                }
-                
-                // Logs component
-                VStack(alignment: .leading) {
-                    Text("Logs")
-                        .font(.title2.bold())
-                }
-                Text("Practice play button")
-                Spacer()
+        ZStack() {
+            if !isSkillAvailable {
+                NoSkillsView()
             }
-            .padding()
         }
     }
 }
