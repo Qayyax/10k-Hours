fcc7f5d09dc4d5f5e2b73ec19cd9dfd1b01a0709
Qayyax <qayyax@gmail.com>
2026-02-12 15:37:04 -0500

    added textfield for the new skill, left with colors

--- 10000 hours/Views/Skills/AddNewSkillView.swift
+++ 10000 hours/Views/Skills/AddNewSkillView.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 struct AddNewSkillView: View {
+    @State private var skillName: String = ""
+    @State private var reasonForLearning: String = ""
     var body: some View {
         ZStack {
             Color.gray
                 .opacity(0.3)
                 .ignoresSafeArea()
             ScrollView {
-                VStack {
+                VStack(spacing: 30) {
                     RectangleView{
                         VStack {
                             HStack(spacing: 20){
@@ -22,11 +24,14 @@ struct AddNewSkillView: View {
                                     .fill(.blue)
                                     .frame(width: 80, height: 80)
                                 // Name and contecxt textfield here
+                                // The text would change based on state
                                 VStack(alignment: .leading) {
-                                    Text("Skill Name")
+                                    Text(skillName.isEmpty ? "Skill Name" : skillName)
                                         .font(.title.bold())
-                                    Text("Why do you want to learn this?")
+                                    Text(reasonForLearning.isEmpty ? "Why do you want to learn this?" : reasonForLearning)
                                         .foregroundStyle(.gray)
+                                        .lineLimit(3)
+                                        .truncationMode(.tail)
                                 }
                                 Spacer()
                             }
@@ -35,6 +40,20 @@ struct AddNewSkillView: View {
                                 .foregroundStyle(.gray)
                                 .padding(.top)
                                 .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    RectangleView{
+                        VStack (alignment: .leading, spacing: 20){
+                            // Skill name
+                            Text("SKILL NAME")
+                            TextField("e.g., Drawing, Piano, Programming", text: $skillName)
+                                
+                            Divider()
+                            
+                            Text("WHY THIS SKILL?")
+                            TextField("What motivates your to master this skill?", text: $reasonForLearning)
+                                
+
                         }
                     }
                 }
